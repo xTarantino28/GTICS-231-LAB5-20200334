65a144524c0fefc0a2d2ef75a0d1096a1fdb8c0b
xTarantino28 <gzanabriaz28@gmail.com>
2023-04-29 22:09:53 -0500

    se puede crear el empleado, se puede borrar (logicamente), funciona la barra de busqueda y el limpiar

--- src/main/java/com/example/gtics231lab520200334/controller/EmpleadoController.java
+++ src/main/java/com/example/gtics231lab520200334/controller/EmpleadoController.java
@@ -10,8 +10,12 @@ import com.example.gtics231lab520200334.repository.JobsRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class EmpleadoController {
@@ -29,8 +33,16 @@ public class EmpleadoController {
 
     @GetMapping("/empleados")
     public String inicioEmpleados(Model model){
-        List<Employees> listaEmpleados = employeesRepository.findAll();
+        List<Employees> listaEmpleados = employeesRepository.listarEmpleados();
         model.addAttribute("listaEmpleados",listaEmpleados);
+        return "empleados/listar";
+    }
+
+    @PostMapping("/empleados/buscar")
+    public String buscarEmpleados(@RequestParam("textoBuscar") String searchText, Model model){
+        List<Employees> listaEmpleados = employeesRepository.buscarEmpleados(searchText);
+        model.addAttribute("listaEmpleados",listaEmpleados);
+        model.addAttribute("textoBuscado",searchText);
         return "empleados/listar";
     }
 
@@ -38,12 +50,51 @@ public class EmpleadoController {
     public String empleadosNuevo(Model model){
         List<Jobs> listaPuestos = jobsRepository.findAll();
         List<Departments> listaDepartamentos = departmentsRepository.findAll();
-        List<Employees> listaJefes = employeesRepository.findAll();
+        List<Employees> listaJefes = employeesRepository.listarEmpleados();
         model.addAttribute("listaPuestos",listaPuestos);
         model.addAttribute("listaJefes",listaJefes);
         model.addAttribute("listaDepartamentos",listaDepartamentos);
         return "empleados/nuevoEmpleado";
     }
+
+    @PostMapping("/empleados/guardar")
+    public String guardarEmpleado(
+           @RequestParam("nombre") String nombre,
+           @RequestParam("apellido") String apellido,
+           @RequestParam("email") String email,
+           @RequestParam("contrasena") String contrasena,
+           @RequestParam("puesto") String puesto_id,
+           @RequestParam("sueldo") double sueldo,
+           @RequestParam("jefe") int jefe_id,
+           @RequestParam("departamento") int departamento_id,
+           RedirectAttributes redirectAttributes
+    ){
+
+        employeesRepository.guardarEmpleado( nombre,  apellido,  email,  contrasena,  puesto_id,  sueldo,  jefe_id,  departamento_id);
+        redirectAttributes.addFlashAttribute("msg","Empleado creado exitosamente");
+        return "redirect:/empleados";
+    }
+
+    @GetMapping("empleados/delete")
+    public String borrarEmpleado(
+            @RequestParam("id") int employee_id,
+            RedirectAttributes redirectAttributes
+    ){
+
+        Optional<Employees> optEmployees = employeesRepository.findById(employee_id);
+
+
+        if (optEmployees.isPresent()) {
+            employeesRepository.borrarEmpleado(employee_id);
+            redirectAttributes.addFlashAttribute("msg","Empleado borrado exitosamente");
+            return "redirect:/empleados";
+        } else {
+            redirectAttributes.addFlashAttribute("msgDanger","No se encontro el empleado");
+            return "redirect:/empleados";
+        }
+
+    }
+
 
 
 
--- src/main/java/com/example/gtics231lab520200334/repository/EmployeesRepository.java
+++ src/main/java/com/example/gtics231lab520200334/repository/EmployeesRepository.java
@@ -2,12 +2,37 @@ package com.example.gtics231lab520200334.repository;
 
 
 import com.example.gtics231lab520200334.entity.Employees;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employees,Integer> {
 
+    @Transactional
+    @Modifying
+    @Query(value = "insert into `hr`.`employees` (`first_name`, `last_name`, `email`, `password`, `job_id`,`salary`, `manager_id`, `department_id`,`enabled` ) values ( ?1,?2,?3,?4,?5,?6,?7,?8,1) ",
+            nativeQuery = true)
+    void guardarEmpleado(String nombre, String apellido, String email, String contrasena, String puesto_id, double sueldo, int jefe_id, int departamento_id);
+
+
+    @Query(value = "SELECT * FROM `hr`.`employees` WHERE enabled = 1",
+            nativeQuery = true)
+    List<Employees> listarEmpleados();
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE `hr`.`employees` SET enabled = 0 where employee_id = ?1",
+            nativeQuery = true)
+    void borrarEmpleado(int employee_id);
+
+    @Query(value = "SELECT e.* FROM employees e left join jobs j on j.job_id = e.job_id  left join departments d on d.department_id = e.department_id left join locations lo on lo.location_id = d.location_id WHERE( (lo.city like %?1%) or (e.first_name Like %?1%) or (e.last_name Like %?1%) or (j.job_title Like %?1%) )  AND enabled = 1",
+            nativeQuery = true)
+    List<Employees> buscarEmpleados(String searchText);
 
 
 }
